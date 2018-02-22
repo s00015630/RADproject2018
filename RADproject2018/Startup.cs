d3cc59fc0ecb7e8108deec1ccdd6cfd20f7ac5f5
mark o shea <s00015630@mail.itsligo.ie>
2018-02-22 13:34:26 +0000

    Add project files.

---
+++ RADproject2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RADproject2018.Startup))]
+namespace RADproject2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
