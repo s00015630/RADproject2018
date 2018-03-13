a61d22ab7fafa9a029661e735fce1c2cd64b6ae4
mark o shea <s00015630@mail.itsligo.ie>
2018-03-13 18:54:43 +0000

    Added student and course api controllers. Updated Gobla.asax.cs file

---
+++ RADproject2018/Controllers/CoursesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RADproject2018.Models;
+
+namespace RADproject2018.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        private StudentDBContext db = new StudentDBContext();
+
+        // GET: api/Courses
+        public IQueryable<Course> GetCourses()
+        {
+            return db.Courses;
+        }
+
+        // GET: api/Courses/5
+        [ResponseType(typeof(Course))]
+        public async Task<IHttpActionResult> GetCourse(int id)
+        {
+            Course course = await db.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        // PUT: api/Courses/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCourse(int id, Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != course.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Courses
+        [ResponseType(typeof(Course))]
+        public async Task<IHttpActionResult> PostCourse(Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Courses.Add(course);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = course.ID }, course);
+        }
+
+        // DELETE: api/Courses/5
+        [ResponseType(typeof(Course))]
+        public async Task<IHttpActionResult> DeleteCourse(int id)
+        {
+            Course course = await db.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            db.Courses.Remove(course);
+            await db.SaveChangesAsync();
+
+            return Ok(course);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CourseExists(int id)
+        {
+            return db.Courses.Count(e => e.ID == id) > 0;
+        }
+    }
+}
--- RADproject2018/Global.asax.cs
+++ RADproject2018/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace RADproject2018
 {
@@ -12,6 +14,7 @@ namespace RADproject2018
     {
         protected void Application_Start() 
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
