eb0eca64c8bf9bcd2be0bd247ef6fd6365636f55
mark o shea <s00015630@mail.itsligo.ie>
2018-03-13 16:59:45 +0000

    Added common objects and A client

---
+++ CommonObjects/DTOObjects.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonObjects
+{
+    public class AssessementDTO
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public int InstructorID { get; set; }
+        public string Descrption { get; set; }
+
+        public CourseDTO Course { get; set; }
+        public InstructorDTO Instructor { get; set; }
+    }
+
+    public class AttendanceDTO
+    {
+        public int ID { get; set; }    
+        public int StudentID { get; set; }      
+        public int SectionID { get; set; }   
+        public DateTime DateAttended { get; set; }
+        public int Hours { get; set; }
+        public StudentDTO Student { get; set; }
+        public SectionDTO Section { get; set; }
+    }
+
+    public class CourseDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Term { get; set; }
+    }
+
+    public class EnrollmentDTO
+    {
+      
+        public int ID { get; set; }
+        public int AcadmaicYear { get; set; }
+        public int Term { get; set; }
+        public DateTime DateEnrolled { get; set; }
+        public int StudentID { get; set; }
+        public int SectionID { get; set; }
+
+        public StudentDTO Student { get; set; }
+        public SectionDTO Section { get; set; }
+
+    }
+
+    public class InstructorDTO
+    {
+        public int ID { get; set; }
+        public int InstructorNum { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Rank { get; set; }
+        public string Type { get; set; }
+
+    }
+
+    public class ScoreDTO
+    {
+        public int ID { get; set; }
+        public int AssessementID { get; set; }
+        public int Grade { get; set; }
+        public int StudentID { get; set; }
+
+        public AssessementDTO Assessment { get; set; }
+        public StudentDTO Student { get; set; }
+        
+    }
+
+    public class SectionDTO
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public int InstrurctorID { get; set; }
+        public int TimeTableID { get; set; }
+
+    }
+
+    public class StudentDTO
+    {
+       
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentNum { get; set; }
+        public string Email { get; set; }
+
+    }
+
+    public class TimeTableDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Day { get; set; }
+        public string Room { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+
+
+
+
+}
---
+++ StudentClient/StudentClient.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using CommonObjects;
+ 
+namespace StudentClient
+{
+    public enum AUTHSTATUS { NONE, OK, INVALID, FAILED }
+     
+
+    public static class ClientApiLib
+    {
+        static public string baseWebAddress;
+        static public string Token = "";
+        static public AUTHSTATUS Status = AUTHSTATUS.NONE;
+
+        static public bool login(string username, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                // Create a from object
+                var content = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("grant_type", "password"),
+                        new KeyValuePair<string, string>("username", username),
+                        new KeyValuePair<string, string>("password", password),
+                    });
+                // Post the form to the Toekn end point
+                var result = client.PostAsync(baseWebAddress + "Token", content).Result;
+                try
+                {
+                    // Make sure System.Net.Http.Extensoins is referenced through Nuget
+                    // For async http action calls
+                    var resultContent = result.Content.ReadAsAsync<Token>(
+                        new[] { new JsonMediaTypeFormatter() }
+                        ).Result;
+                    string ServerError = string.Empty;
+                    if (!(String.IsNullOrEmpty(resultContent.AccessToken)))
+                    {
+                        Console.WriteLine(resultContent.AccessToken);
+                        Token = resultContent.AccessToken;
+                        Status = AUTHSTATUS.OK;
+                        return true;
+                    }
+                    else
+                    {
+                        Token = "Invalid Login";
+                        Status = AUTHSTATUS.INVALID;
+                        Console.WriteLine("Invalid credentials");
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Status = AUTHSTATUS.FAILED;
+                    Token = "Server Error -> " + ex.Message;
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+
+            }
+        }
+
+        static public List<StudentDTO> geStudentList()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                // Insert the Bearer Token Header
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                var response = client.GetAsync(baseWebAddress + "api/Students").Result;
+                // Newtonsoft Json deserialiser Will attempt to Match the fields in the DTO 
+                // against the Student Object passed back from the ReorderList Method
+                var resultContent = response.Content.ReadAsAsync<List<StudentDTO>>(
+                    new[] { new JsonMediaTypeFormatter() }).Result;
+                return resultContent;
+            }
+        }
+
+        static public StudentDTO addStudent(StudentDTO student)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                var response = client.PostAsJsonAsync(baseWebAddress + "api/Students/add/Course", student).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var resultContent = response.Content.ReadAsAsync<StudentDTO>(
+                        new[] { new JsonMediaTypeFormatter() }).Result;
+
+                    return resultContent;
+                }
+                return null;
+            }
+        }
+
+        static public CourseDTO delete(int courseID)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                var response = client.DeleteAsync(baseWebAddress + "api/Students/delete/Course/" + courseID).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var resultContent = response.Content.ReadAsAsync<CourseDTO>(
+                        new[] { new JsonMediaTypeFormatter() }).Result;
+
+                    return resultContent;
+                }
+                return null;
+            }
+        }
+
+    }
+}
